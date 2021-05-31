9c9c6d7744b42ceb858eb71e8a442603123cba80
dreamlancer <dreamlancer69@gmail.com>
2021-05-31 06:28:44 -0700

    by bohdan

---
+++ src/api/TableStyle.tsx
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+
+interface TableStyleProps {
+  pointer?: boolean;
+}
+export const TableStyle = styled.div<TableStyleProps>`
+  ${({ pointer }) =>
+    pointer &&
+    css`
+      .ant-table-row {
+        cursor: pointer;
+      }
+    `}
+
+  .ant-table-thead > tr > th {
+    color: #fff;
+    background: rgba(8, 81, 161, 0.9);
+    font-weight: bold;
+  }
+  .ant-table-tbody > tr.ant-table-row:hover > td {
+    background: #e1e1e1;
+  }
+  .ant-table-tbody > tr.ant-table-row:nth-child(odd) {
+    background-color: #d0d0d0;
+  }
+  .waiting {
+    td:last-child {
+      color: red;
+    }
+  }
+`;
